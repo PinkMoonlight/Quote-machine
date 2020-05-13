5c6894e8a17c4b0fc4ade457a9c09299e4fc07c1
Pink Moonlight <kate.cherie25@gmail.com>
2020-05-13 15:22:49 -0700

    wrote basic structure and css, and begun working on fetching data with API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import Quotebox from './Quotebox.js'
 import './App.css';
 
-function App() {
+class App extends Component {
+
+    state = {
+      loading: true,
+      quotes: []
+    };
+  
+
+  componentDidMount() {
+    let quoteAPI;
+    /* Fetch  quote API */
+        fetch("https://type.fit/api/quotes")
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          quoteAPI = Array.from(data);
+            console.log(quoteAPI);
+            console.log(quoteAPI[0]);
+        })
+    this.setState({quotes: quoteAPI});
+    console.log(this.state.quotes[0]);
+  };
+
+  onClick = e => {
+    
+  };
+
+  
+render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="quote-box">
+          <div className="display-quote">
+            <p id="quote-text"><span className="quotemark">&#34;</span> Quote <span className="quotemark">&#34;</span></p>
+              <span id="quote-author">~ Author</span>
+          </div>
+          <div className="interactions">
+            <a href="http://www.twitter.com/intent/tweet" target="_blank" id="tweet-quote"><img className="twitter-icon" src="twitter.png" alt="Twitter Logo" /></a>  
+            <button id="new-quote">New Quote</button>
+          </div>
+        </div>
       </header>
+      <footer className="footer">
+        <p>Built by <a className="name-link" href="#">Kate Fisher</a></p>
+      </footer>
     </div>
   );
+}
+
 }
 
 export default App;
