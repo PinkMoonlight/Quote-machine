b4b4728a342707038986d77a428ba806e1b910aa
Pink Moonlight <kate.cherie25@gmail.com>
2020-05-13 16:59:39 -0700

    Updates

---
+++ precache-manifest.73029b2b6fa9cb5300f08ac4f6534d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2f0bb7a4c6a7288a5b5f484b4e73a5",
+    "url": "/Quote-machine/index.html"
+  },
+  {
+    "revision": "7d23df0d247c76039d39",
+    "url": "/Quote-machine/static/css/main.4687f6e9.chunk.css"
+  },
+  {
+    "revision": "1aec0849c0a0afc95168",
+    "url": "/Quote-machine/static/js/2.178efd61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quote-machine/static/js/2.178efd61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d23df0d247c76039d39",
+    "url": "/Quote-machine/static/js/main.763ad0e8.chunk.js"
+  },
+  {
+    "revision": "9ad1a79d7ba9f9508ac7",
+    "url": "/Quote-machine/static/js/runtime-main.7e36d2f0.js"
+  }
+]);
