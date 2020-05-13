6af7837e30ef62c8bbad8ed34dd411ac9ca9faf0
Pink Moonlight <kate.cherie25@gmail.com>
2020-05-13 16:26:46 -0700

    got app functionality working to generate quotes

--- src/App.js
+++ src/App.js
@@ -7,52 +7,59 @@ class App extends Component {
 
     state = {
       loading: true,
-      quotes: []
+      current: 0,
+      quote: null,
+      author: null
     };
+
+ /* Fetch  quote API */
+    async getQuote() {
+      let current = this.state.current;
+
+      const url = "https://type.fit/api/quotes";
+      const response = await fetch(url);
+      const data = await response.json();
+      const dataArray = [...data];
+
+      this.setState( {
+        quote: dataArray[current].text,
+        author: dataArray[current].author, 
+        loading: false, 
+        current: current + 1} );
+        console.log(this.state.current);
+  }
   
 
-  componentDidMount() {
-    let quoteAPI;
-    /* Fetch  quote API */
-        fetch("https://type.fit/api/quotes")
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(data) {
-          quoteAPI = Array.from(data);
-            console.log(quoteAPI);
-            console.log(quoteAPI[0]);
-        })
-    this.setState({quotes: quoteAPI});
-    console.log(this.state.quotes[0]);
+  async componentDidMount() {
+    this.getQuote();
   };
 
-  onClick = e => {
-    
-  };
 
+   onClick = () => {
+    this.getQuote()
+  };
   
-render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div id="quote-box">
-          <div className="display-quote">
-            <p id="quote-text"><span className="quotemark">&#34;</span> Quote <span className="quotemark">&#34;</span></p>
-              <span id="quote-author">~ Author</span>
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div id="quote-box">
+            <div className="display-quote">
+              <p id="text"><span className="quotemark">&#34;</span> {this.state.quote} <span className="quotemark">&#34;</span></p>
+                <span id="author">~ {this.state.author}</span>
+            </div>
+            <div className="interactions">
+              <a href="http://www.twitter.com/intent/tweet" target="_blank" id="tweet-quote"><img className="twitter-icon" src="twitter.png" alt="Twitter Logo" /></a>  
+              <button id="new-quote" onClick={this.onClick} >New Quote</button>
+            </div>
           </div>
-          <div className="interactions">
-            <a href="http://www.twitter.com/intent/tweet" target="_blank" id="tweet-quote"><img className="twitter-icon" src="twitter.png" alt="Twitter Logo" /></a>  
-            <button id="new-quote">New Quote</button>
-          </div>
-        </div>
-      </header>
-      <footer className="footer">
-        <p>Built by <a className="name-link" href="#">Kate Fisher</a></p>
-      </footer>
-    </div>
-  );
-}
+        </header>
+        <footer className="footer">
+          <p>Built by <a className="name-link" href="#">Kate Fisher</a></p>
+        </footer>
+      </div>
+    );
+  }
 
 }
 
